de17477a954fcecdbd845a52c15c46dfffd5ba4c
Edmond Cote <edmond.cote@gmail.com>
2016-06-03 17:50:41 -0700

    solution cleanup

--- ch9_1.cpp
+++ ch9_1.cpp
@@ -1,5 +1,5 @@
-// ch9.1 : write method to count # of ways a child can run up the stairs, given
-//         that child can hop either 1, 2, or 3 n
+// ch9.1 : given staircase of n steps, count # ways child can run up stairs given
+//         child can hop either 1, 2, or 3 steps
 
 #include <iostream>
 #include <list>
@@ -9,23 +9,27 @@
 using namespace std;
 
 /*
-try with top-down recursion
+HINTS:
+- child can reach n-th step from step(n-1), step(n-2), or step(n-3) ** this means sum **
+- this is similar to fibonacci
 
-f(1) = 1
-f(2) = f(1) + 1 = 2
-f(3) = f(2) + 1 = 4
-f(4) = f(3) + f(2) + f(1) -- **child can reach the 4th floor, from either floor 1,2,3**
-f(n) = f(n-1)
-
-Total number of ways reaching the last step is the sum of the number of ways reaching each of the last three steps.
-
+f(0) == 0
+f(1) == 1
+f(2) == 2
+f(3) == 4
+f(4) == 1+2+4=7
+f(4) == 2+4+7=13
 */
 
 int count_ways(int n) {
     if (n < 0)
         return 0;
-    else if (n == 0)
+    else if (n == 0 || n == 1)
         return 1;
+    else if (n == 2)
+        return 2;
+    else if (n == 3)
+        return 4;
     else
         return count_ways(n-3) + count_ways(n-2) + count_ways(n-1);
 }
@@ -36,5 +40,6 @@ TEST(ch9_1, basic) {
     ASSERT_EQ(count_ways(2), 2);
     ASSERT_EQ(count_ways(3), 4);
     ASSERT_EQ(count_ways(4), 7);
+    ASSERT_EQ(count_ways(5), 13);
 }
 

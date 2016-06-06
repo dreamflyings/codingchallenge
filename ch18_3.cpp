68818b485d537aef0c7d4fd7f024df2a2a7ac927
Edmond Cote <edmond.cote@gmail.com>
2016-06-06 09:23:39 -0700

    use map for solution

--- ch18_3.cpp
+++ ch18_3.cpp
@@ -3,6 +3,7 @@
 //         be repeated)
 
 #include <iostream>
+#include <map>
 #include <vector>
 #include <stdlib.h>
 #include <time.h>
@@ -11,23 +12,33 @@
 
 using namespace std;
 
-/*
-    I am going to use a map for this.
-*/
+// I am using a map to avoid repeated elements. Each element should have equal
+// probability of being chosen.  The solution provides a better answer, but 
+// it was confusing to me
 
 void choose_m_from_n(vector<int> &m, vector<int> &n) {
-    for (unsigned i=0; i<n; i++) {
+    map<int, int> prev_chosen;
+    int j = 0;
+    for (unsigned i=0; i<m.size(); i++) {
+        int which;
+        do {
+            which = rand() % n.size();
+        } while (prev_chosen[which] != 0);
+        m[j++] = n[which];
+        prev_chosen[which] = 1;
     }
 }
 
 TEST(ch18_3, basic) {
     srand(time(NULL)); // randomize seed
 
-    vector<int> n(4);
+    vector<int> n(5);
     for (unsigned i=0; i<n.size(); i++)
         n[i] = i;
 
-    vector<int> m(2);
+    vector<int> m(4);
 
-    choose_m_from_n(n, m);
+    choose_m_from_n(m, n);
+
+    //--FIXME: make self checking
 }

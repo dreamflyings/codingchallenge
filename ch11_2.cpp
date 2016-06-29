bbe04d10193127e8f0e9379fb2355050f276bd35
Edmond Cote <edmond.cote@gmail.com>
2016-06-29 11:06:33 -0700

    working solution, not efficient

--- ch11_2.cpp
+++ ch11_2.cpp
@@ -2,6 +2,8 @@
 //         are next to each other
 
 #include <iostream>
+#include <algorithm>
+#include <string>
 #include <vector>
 
 #include <gtest/gtest.h>
@@ -10,19 +12,42 @@ using namespace std;
 
 namespace ch11_2 {
 
+int is_anagram(const string &a, const string &b) {
+    string a_tmp = a;
+    string b_tmp = b;
+
+    std::sort(a_tmp.begin(), a_tmp.end());
+    std::sort(b_tmp.begin(), b_tmp.end());
+    return (a_tmp == b_tmp) ? 1 : 0;
+}
+
+// this is far from the most effective algorithm ...
 void string_sort(vector<string> &v) {
-    for (unsigned i=0; i<v.size(); i++) {
-        cout << v[i] << endl;
+    for (unsigned i=0; i<v.size()-1; i++) {
+        for (unsigned j=i+1; j<v.size(); j++) {
+            if (is_anagram(v[i], v[j])) {
+                // swap i+1 and j
+                string tmp;
+                tmp = v[j];
+                v[j] = v[i+1];
+                v[i+1] = tmp;
+            }
+        }
     }
 }
 
 TEST(ch11_2, basic) {
-    vector<string> v;
-    v.push_back("hello");
-    v.push_back("iceman");
-    v.push_back("world");
-    v.push_back("cinema");
+    vector<string> v = {"hello", "iceman", "world", "cinema"};
+    vector<string> exp = {"hello", "iceman", "cinema", "world"};
     string_sort(v);
+    ASSERT_EQ(v, exp);
+}
+
+TEST(ch11_2, two_anagrams) {
+    vector<string> v = {"a", "b", "ab", "cd", "ab", "cd"};
+    vector<string> exp = {"a", "b", "ab", "ab", "cd", "cd"};
+    string_sort(v);
+    ASSERT_EQ(v, exp);
 }
 
 } // namespace ch11_2

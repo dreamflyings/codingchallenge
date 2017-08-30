393b1d5fab68b8fa464f874a6d52171993f75d35
Edmond Cote <edmond.cote@gmail.com>
2017-08-30 09:49:33 -0700

    Initial version

---
+++ ctci/ch17_5.cpp
@@ -0,0 +1,16 @@
+// ch17.5: Master mind
+
+#include <stdio.h>
+#include <array>
+#include <vector>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+namespace ch17_5 {
+
+TEST(ch17_5, basic) {
+  ASSERT_TRUE(0);
+}
+
+}

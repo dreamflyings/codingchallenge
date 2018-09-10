22eb1f3477ee09fbbba5a6d29a72cbb340a26d2e
Edmond Cote <edmond.cote@gmail.com>
2018-09-10 10:49:25 -0700

    solved, minus overflow check

---
+++ leetcode/python/reverse_integer.py
@@ -0,0 +1,24 @@
+"""
+### Problem ###
+https://leetcode.com/problems/reverse-integer/description/
+
+### Notes ###
+I did not consider the overflow case.
+
+"""
+
+class Solution(object):
+    def reverse(self, x):
+        is_negative = True if x < 0 else False
+        x = -x if is_negative else x
+
+        result = 0
+
+        while x != 0:
+            mod = x % 10
+            # FIXME: predict overflow
+            result = result*10 + mod
+            x = int(x / 10)
+
+        return -result if is_negative else result
+

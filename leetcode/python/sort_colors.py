85a37e5122d05d4d7211d1bd0e3e82d01ae4aa40
Edmond Cote <edmond.cote@gmail.com>
2018-09-19 09:03:15 -0700

    solved

--- leetcode/python/sort_colors.py
+++ leetcode/python/sort_colors.py
@@ -1,7 +1,6 @@
 """
 ### Problem ###
 
-
 https://leetcode.com/explore/interview/card/top-interview-questions-medium/110/sorting-and-searching/798/
 
 Given an array with n objects colored red, white or blue, sort them in-place so
@@ -31,41 +30,42 @@ A rather straight forward solution is a two-pass algorithm using counting sort.
 
 Two pass algorithm is easy.  It is described above.
 
+MARK index OF last ONE
+
+i nums nums[i] l nums' l'
+0 120  1       - 120   0 # mark index of last one
+1 120  2       0 120   0 # pop 2, insert after last one
+2 120  0       1 012     # pop 0, insert at head, l++
+
 Input:  [2,0,2,1,1,0]
 Output: [0,0,1,1,2,2]
 
-i nums    nums[i] action
-0 [1,2,0] 1       -
-1 [1,2,0] 2       move to back
-1 [1,0,2] 0       move to front
-2 [0,1,2] end, do nothing 
+
+i nums    nums[i]  l | nums'   l'
+0 202110  2       -1 | 202110  -1
+1 202110  0       -1 | 022110  -1
+2         2
+3         1
+4         1
+5         0
+
+Input: [1,2,1]
+Output: [1,1,2]
 
 
-[1, 2, 0] 0 False False
-
-[1, 2, 0] 1 False True
-[1, 0, 2] 1 True False
-
-[0, 1, 2] 2 False True
-[0, 1, 2] 2 False True
+i nums num l | nums' l'
+0 121  1   0   121   0  before first one   -> pop(0), insert(0, 1)
+1 121  2       121   1  insert after first one -> pop(1), insert(0, 2)
+2 121  1       112      insert before after one -> pop(1), insert(0, 1), l++
 
 
-MARK the LAST zero
+Input: [1,2,0]
+Output: [0,1,2]
 
-
-
-i nums l nums' l'
-0 120  0 120   0
-1 120  0 120   1
-2 120  1 012
-
-1 2 0 l=0
-2 1 0 l++
-
-
-
-
-
+i nums num l | nums' l'
+0 120  1   0 | 120   0 before first one -> pop(0), insert(0, 1) // index of insert <= i
+1 120  2     | 120   0 after first one -> pop(1), insert(0, 2)
+2 120  0     |         at heat -> pop(0), insert(0,0
 
 """
 
@@ -75,28 +75,37 @@ import unittest
 class SortColorsTest(unittest.TestCase):
     def sort_colors(self, nums):
         i = 0
-        while i < len(nums)-1:
+        l = 0
+
+        while i < len(nums):
             num = nums[i]
             is_zero = num == 0
+            is_one = num == 1
             is_two = num == 2
-            print(nums, i, is_zero, is_two)
+
+            nums.pop(i)
 
             if is_zero:
-                nums.pop(i)
+                # insert at head
                 nums.insert(0, 0)
-                i += 1
+                l += 1
             elif is_two:
-                nums.pop(i)
-                nums.append(2)
-            else:
-                i += 1
+                # insert after first one
+                nums.insert(l, 2)
+            elif is_one:
+                # insert after first one
+                nums.insert(l, 1)
+                l += 1
+
+            i += 1
         return nums
 
-             
-
     def test_basic(self):
-        self.assertEqual([0,0,1,1,2,2], self.sort_colors([2,0,2,1,1,0]))
+        self.assertEqual([0, 0, 1, 1, 2, 2], self.sort_colors([2, 0, 2, 1, 1, 0]))
 
-    def test_corner(self):
-        self.assertEqual([0,1,2], self.sort_colors([1,2,0]))
+    def test_corner_2(self):
+        self.assertEqual([0, 1, 2], self.sort_colors([1, 2, 0]))
+
+    def test_corner_2(self):
+        self.assertEqual([1, 1, 2], self.sort_colors([1, 2, 1]))
 

391d9bee2abc8fcfb29cea5673e3405c37d81538
Edmond Cote <edmond.cote@gmail.com>
2018-09-24 21:06:19 -0700

    solved

---
+++ leetcode/python/find_kth_largest.py
@@ -0,0 +1,59 @@
+"""
+### Problem ###
+
+Kth Largest Element in an Array
+
+https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+https://leetcode.com/explore/interview/card/top-interview-questions-medium/110/sorting-and-searching/800/
+
+Find the kth largest element in an unsorted array. Note that it is the kth
+largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+
+Example 2:
+
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+
+### Notes ###
+
+sorted is O(n*logn), not the greatest solution :(
+
+use heapq instead?
+
+heap is a special case of a balanced binary tree
+
+the min-heap property: the value of each node is greater than or equal to the
+value of its parent, with the minimum-value element at the root.
+
+its left child is located at 2*k index 
+its right child is located at 2*k+1. index 
+its parent is located at k/2 index
+
+"""
+
+import unittest
+
+import heapq
+
+class FindKthLargestTest(unittest.TestCase):
+    def find_kth_largest(self, nums, k):
+        #num_nums = len(nums)
+        #return sorted(nums)[num_nums-k]
+        heapq.heapify(nums)
+        return heapq.nlargest(k, nums)[-1]
+
+    def test_example_1(self):
+        self.assertEqual(5, self.find_kth_largest([3,2,1,5,6,4], 2)) # 1,2,3,4, 5,6
+
+    def test_example_2(self):
+        self.assertEqual(4, self.find_kth_largest([3,2,3,1,2,4,5,5,6], 4)) # 1,2,2,3,3, 4,5,5,6
+        # heapify -> [1, 2, 3, 2, 3, 4, 5, 5, 6]

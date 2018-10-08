11f992b2db03fdcdf0585db4f132a4e82fb99f4e
Edmond Cote <edmond.cote@gmail.com>
2018-10-08 09:57:35 -0700

    solved

---
+++ leetcode/python/containts_duplicate.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution:
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        d = {}
+        for n in nums:
+            if not n in d:
+                d[n] = 1
+            else:
+                return True
+        return False
+
---
+++ leetcode/python/first_uniq_char.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/first-unique-character-in-a-string/description/
+
+from collections import OrderedDict
+
+class Solution:
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s == "":
+            return -1
+        d = OrderedDict()
+
+        pos = 0
+        for c in s:
+            if not c in d:
+                d[c] = (0, pos)
+            else:
+                d[c] = (d[c][0]+1, pos)
+            pos += 1
+
+        for k,v in d.items():
+            if d[k][0] == 0:
+                return d[k][1]
+
+        return -1
+
